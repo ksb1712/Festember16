b1783d05e94c83c78d4b3afbaf44621ada5fbce8
ksb1712 <bharath.sreedhar.k@gmail.com>
2016-08-26 23:14:10 +0530

    Login completed

--- app/src/main/java/com/festember16/app/MainActivity.java
+++ app/src/main/java/com/festember16/app/MainActivity.java
@@ -1,13 +1,18 @@
 package com.festember16.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(com.festember16.app.festember16.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(this,LoginActivity.class);
+        startActivity(i);
     }
 }
