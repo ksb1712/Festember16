85e6db988f4c950e2cc5c25b6029444f14745f50
ksb1712 <bharath.sreedhar.k@gmail.com>
2016-09-16 10:19:13 +0530

    Upgraded Events List activity

---
+++ app/src/main/java/com/festember16/app/Events_list.java
@@ -0,0 +1,67 @@
+package com.festember16.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Events_list extends AppCompatActivity {
+    private GridLayoutManager lLayout;
+    String s[] = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_events_list);
+        setTitle(null);
+        s = getIntent().getExtras().getStringArray("events");
+
+        List<ItemObject> rowListItem = getAllItemList();
+        lLayout = new GridLayoutManager(Events_list.this, 2);
+
+        RecyclerView rView = (RecyclerView)findViewById(R.id.recycler_view);
+        rView.setHasFixedSize(true);
+        rView.setLayoutManager(lLayout);
+
+        RecyclerViewAdapter2 rcAdapter = new RecyclerViewAdapter2(Events_list.this, rowListItem);
+        rView.setAdapter(rcAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<ItemObject> getAllItemList(){
+
+        List<ItemObject> allItems = new ArrayList<ItemObject>();
+        for(int i = 0; i < s.length;i++)
+            allItems.add(new ItemObject(s[i]));
+
+
+        return allItems;
+    }
+
+}
